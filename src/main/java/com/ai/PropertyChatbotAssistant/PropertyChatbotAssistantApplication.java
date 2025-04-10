acb325ed573ecbc545e285081e303a61efb786bc
Hepi <hepichaniyara@gmail.com>
2025-04-10 19:02:34 +0530

    initial commit

---
+++ src/main/java/com/ai/PropertyChatbotAssistant/PropertyChatbotAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.ai.PropertyChatbotAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyChatbotAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyChatbotAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/PropertyChatbotAssistant/PropertyChatbotAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.PropertyChatbotAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyChatbotAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
